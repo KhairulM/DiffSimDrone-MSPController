5f9445c8d563a5f776bee24e5f163df65bfde8bc
Khairul Makirin <khairulmmakirin@gmail.com>
2026-01-27 19:48:50 +0900

    fix: change resolution

--- realsense_onnx_inference.py
+++ realsense_onnx_inference.py
@@ -97,7 +97,7 @@ def main() -> None:
 
     pipeline = rs.pipeline()
     config = rs.config()
-    config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
     profile = pipeline.start(config)
     depth_sensor = profile.get_device().first_depth_sensor()
     depth_scale = depth_sensor.get_depth_scale()
